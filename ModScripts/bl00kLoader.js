d1cc6add3f2c4c568f29a93ae593dec1871ab464
Jamison Smith <102773780+Jamison-smith@users.noreply.github.com>
2023-10-23 15:22:40 -0700

    Update bl00kLoader.js

--- ModScripts/bl00kLoader.js
+++ ModScripts/bl00kLoader.js
@@ -1,6 +1,7 @@
 function bl00kget() { 
     fetch('ModScripts/bl00ks.json').then((response) => response.json()).then((json) => {
         var js = json;
+        js = JSON.stringify(js)
         js = JSON.parse(js)
         alert(js.Meme-001.Name);
     });
