217b2f681622a887bed758dc1b72a5adb85dc687
Jamison Smith <102773780+Jamison-smith@users.noreply.github.com>
2023-10-23 12:40:19 -0700

    Update bl00kLoader.js

--- ModScripts/bl00kLoader.js
+++ ModScripts/bl00kLoader.js
@@ -1,3 +1,3 @@
 function bl00kget() { 
-    fetch('./bl00ks.json').then((response) => response.json()).then((json) => alert(JSON.parse(json)));
+    fetch('./bl00ks.json').then((response) => response.json()).then((json) => alert("d")));
 }
